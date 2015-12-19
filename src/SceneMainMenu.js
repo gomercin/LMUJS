96c644fa22aa5cdd6fe4286ea3b4579db0a86013
gomercin <gomercin@gmail.com>
2015-12-19 23:28:29 +0200

    creating initial structure

---
+++ src/SceneMainMenu.js
@@ -0,0 +1,3 @@
+/**
+ * Created by GomerciN on 19.12.2015.
+ */
---
+++ src/resource.js
@@ -0,0 +1,17 @@
+var res = {
+    imgBackground : "res/bgb.jpg",
+    imgBlank : "res/blank.png",
+    imgStartColoredGame : "res/coloredgame.png",
+    imgStartPhotoGame : "res/fotosec.png",
+    imgSquare : "res/kare.png",
+    imgPeek : "res/peek.png",
+    imgTutorial1 : "res/tutorial1.jpg",
+    imgTutorial2 : "res/tutorial2.jpg",
+    imgTutorial3 : "res/tutorial2.jpg",
+    imgUndo : "res/undo.png"
+};
+
+var g_resources = [];
+for (var i in res) {
+    g_resources.push(res[i]);
+}
